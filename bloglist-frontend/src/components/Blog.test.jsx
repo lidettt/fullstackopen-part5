67d9686e8020a318163f602c825b187120fb90dd
Lidet Voeun <lidetvoeun@gmail.com>
2026-01-28 19:01:39 +0700

    5.14: Blog List Tests, step 2

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Blog from "./Blog";
+import userEvent from "@testing-library/user-event";
 
 test("renders content", () => {
   const blog = {
@@ -20,4 +21,34 @@ test("renders content", () => {
   expect(url).toBeNull();
   const likes = screen.queryByText("10");
   expect(likes).toBeNull();
+});
+
+test("shows url and likes when view button is clicked", async () => {
+  const blog = {
+    id: "1",
+    title: "Testing the blog component",
+    author: "Lidet Tester",
+    url: "https://example.com",
+    likes: 10,
+  };
+
+  const mockToggle = vi.fn();
+
+  // render with expandedId set to null (collapsed)
+  const { rerender } = render(
+    <Blog blog={blog} expandedId={null} onToggle={mockToggle} />,
+  );
+
+  //find and await for user to click the button
+  const user = userEvent.setup();
+  const button = screen.getByText("view");
+  await user.click(button);
+
+  //re-render after the user click the button to change the expandedId to blog.id instead of null
+  rerender(<Blog blog={blog} expandedId={blog.id} onToggle={mockToggle} />);
+
+  const url = screen.queryByText("https://example.com");
+  expect(url).toBeDefined();
+  const likes = screen.queryByText("10");
+  expect(likes).toBeDefined();
 });
