b482f3504b0b856accef5121fc84dbd8ef468af2
Lidet Voeun <lidetvoeun@gmail.com>
2026-07-27 22:17:17 +0700

    5.18: Blog List End To End Testing, step 2

--- blog-list-end-to-end-testing/tests/blogs-app.spec.js
+++ blog-list-end-to-end-testing/tests/blogs-app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post("http://localhost:3003/api/testing/reset");
+    await request.post("http://localhost:3003/api/users", {
+      data: {
+        username: "lidet-test",
+        name: "Voeun Chanlidet",
+        password: "lidet123",
+      },
+    });
+
     await page.goto("http://localhost:5173");
   });
 
@@ -15,5 +24,24 @@ describe("Blog app", () => {
     await expect(usernameLabel).toBeVisible();
     await expect(passwordLabel).toBeVisible();
     await expect(loginButton).toBeVisible();
+  });
+
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await page.getByLabel("username").fill("lidet-test");
+      await page.getByLabel("password").fill("lidet123");
+      await page.getByRole("button", { name: "login" }).click();
+
+      await expect(page.getByText("Voeun Chanlidet logged in")).toBeVisible();
+    });
+
+    test("fails with wrong credentials", async ({ page }) => {
+      await page.getByLabel("username").fill("lidet-test");
+      await page.getByLabel("password").fill("wrong");
+      await page.getByRole("button", { name: "login" }).click();
+
+      const errorDiv = page.locator(".error");
+      await expect(errorDiv).toContainText("wrong username or password");
+    });
   });
 });
