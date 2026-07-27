6a8589cd693e76314eb9684ec958f7ab82a5a86c
Lidet Voeun <lidetvoeun@gmail.com>
2026-07-27 22:38:12 +0700

    5.19: Blog List End To End Testing, step 3

--- blog-list-end-to-end-testing/tests/blogs-app.spec.js
+++ blog-list-end-to-end-testing/tests/blogs-app.spec.js
@@ -44,4 +44,24 @@ describe("Blog app", () => {
       await expect(errorDiv).toContainText("wrong username or password");
     });
   });
+
+  describe("When logged in", () => {
+    beforeEach(async ({ page }) => {
+      await page.getByLabel("username").fill("lidet-test");
+      await page.getByLabel("password").fill("lidet123");
+      await page.getByRole("button", { name: "login" }).click();
+    });
+
+    test("a new blog can be created", async ({ page }) => {
+      await page.getByRole("button", { name: "create new blog" }).click();
+      await page.getByLabel("title").fill("new blog can be created");
+      await page.getByLabel("author").fill("lidet");
+      await page.getByLabel("url").fill("newblog.com");
+      await page.getByRole("button", { name: "create" }).click();
+
+      await expect(
+        page.getByText("new blog can be created lidet"),
+      ).toBeVisible();
+    });
+  });
 });
